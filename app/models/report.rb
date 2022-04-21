78b408b035a0261fb45c3ae4f2331ee806c02893
shadowdedulet <shadow_dedulet@mail.ru>
2022-04-21 18:55:04 +0300

    added enums to Report

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,16 @@
 class Report < ApplicationRecord
+  enum report_type: {
+    by_price: 1,
+    by_capacity: 2,
+    by_vols_amount: 3,
+    by_vols_capacity: 4
+  }
+
+  enum unit_type: {
+    cpu: 1,
+    ram: 2,
+    ssd: 3,
+    sata: 4,
+    sas: 5
+  }
 end
